c9873f021d96db2476dbf45a68cdf7e5ef6924fc
Tore Skogøy <tore.skogoy@servere.no>
2019-08-19 20:17:06 +0200

    Added Licenses.ps1

--- Licenses/Licenses.ps1
+++ Licenses/Licenses.ps1
@@ -1,4 +1,13 @@
 # This is a collection of various commands regarding licenses in Office 365
 
+#Create Credential Object
+$Username = "AdminEmail"
+$Password = ConvertTo-SecureString "Password" -AsPlainText -Force
+$Credential = New-Object System.Management.Automation.PSCredential ($Username, $Password)
+
 #Connect to MS Online Service
+Connect-MsolService -Credential $Credential
+
+#Connect to Azure AD
+Connect-AzureAD -Credential $Credential
 
